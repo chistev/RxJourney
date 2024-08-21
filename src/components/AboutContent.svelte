014c1872309c8dcf8151070a0e581d95a9c6d88c
chistev <stephenowabie@gmail.com>
2024-08-21 15:36:23 +0100

    updated AboutMe component with Gmail icon added

--- src/components/AboutContent.svelte
+++ src/components/AboutContent.svelte
@@ -1,40 +1,41 @@
-<script>
-    // Logic if needed
-  </script>
-  
-  <style>
-    .about-content {
-      /* Add necessary styling here */
-    }
-  
-    .twitter-button {
-      display: inline-flex;
-      align-items: center;
-      padding: 10px 20px;
-      background-color: #14171A;
-      color: white;
-      text-decoration: none;
-      border-radius: 5px;
-      font-weight: bold;
-      margin-top: 20px;
-    }
-  
-    .twitter-button i {
-      margin-right: 10px;
-      font-size: 1.2em;
-    }
-  </style>
-  
-  <div class="about-content">
-    <h2>About Me</h2>
-    <p>
-      I am currently an intern pharmacist navigating the complexities of healthcare and medication management. Alongside my passion for Pharmacy, I am also a dedicated full-stack web developer, constantly honing my skills in building dynamic and responsive web applications.
-    </p>
-    <p>
-      My journey into web development has been fueled by a desire to create and innovate, seeking to merge my understanding of healthcare with the power of technology. My ultimate goal is to transition fully into tech, using my coding skills to escape the matrix and make a broader impact.
-    </p>
-    <a href="https://twitter.com/chistev12" target="_blank" class="twitter-button">
-      <i class="bi bi-twitter-x"></i> Contact me    
-    </a>
-  </div>
-  
+<style>
+  .twitter-button, .email-button {
+    display: inline-flex;
+    align-items: center;
+    padding: 10px 20px;
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+    font-weight: bold;
+    margin-top: 20px;
+  }
+
+  .twitter-button {
+    background-color: #14171A;
+  }
+
+  .email-button {
+    background-color: #DB4437;
+  }
+
+  .twitter-button i, .email-button i {
+    margin-right: 10px;
+    font-size: 1.2em;
+  }
+</style>
+
+<div class="about-content">
+  <h2>About Me</h2>
+  <p>
+    I am currently an intern pharmacist. Alongside my passion for Pharmacy, I am also a dedicated full-stack web developer, constantly honing my skills in building dynamic and responsive web applications.
+  </p>
+  <p>
+    My journey into web development has been fueled by a desire to create and innovate. My ultimate goal is to transition fully into tech, using my coding skills to escape the matrix and make a broader impact.
+  </p>
+  <a href="https://twitter.com/chistev12" target="_blank" class="twitter-button">
+    <i class="bi bi-twitter-x"></i> Contact me    
+  </a>
+  <a href="mailto:chistev12@gmail.com" class="email-button">
+    <i class="bi bi-google"></i> Email me    
+  </a>
+</div>
