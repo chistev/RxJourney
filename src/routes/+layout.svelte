7b2fc9d20e7eb52cb2040e39c2d4eff46e6f755a
chistev <stephenowabie@gmail.com>
2025-01-17 17:15:11 +0100

    trying to fix meta tags and prevent bugs when clicking on a recommended post

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,3 @@
 <Navbar />
 
 <slot></slot>
-
-<!-- The head block allows individual pages to set their own title and meta tags -->
-<svelte:head>
-  <!-- Default title and meta description in case the page does not provide one -->
-  <title>RxJourney</title>
-  <meta name="description" content="The Journey of an Intern Pharmacist" />
-</svelte:head>
