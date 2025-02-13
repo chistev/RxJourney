1f24d1e89ceba7d6bad14465ee0a564f25755ab0
chistev <stephenowabie@gmail.com>
2025-02-13 14:41:25 +0100

    removed javascript buttons that served as links

--- src/components/HomeContent.svelte
+++ src/components/HomeContent.svelte
@@ -1,11 +1,7 @@
 <script>
-	import ArticleList from './ArticleList.svelte';
+  import ArticleList from './ArticleList.svelte';
 
   export let posts;
-
-  function handlePostClick(postSlug) {
-    window.location.href = `/${postSlug}`;
-  }
 </script>
 
-<ArticleList {posts} onPostClick={handlePostClick} />
+<ArticleList {posts} />
