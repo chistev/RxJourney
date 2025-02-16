ab3dd685fc9ea6e9bd3779c59c680764b5cd5472
chistev <stephenowabie@gmail.com>
2025-02-16 11:41:05 +0100

    added see more button to the HomeContent

--- src/components/HomeContent.svelte
+++ src/components/HomeContent.svelte
@@ -5,3 +5,27 @@
 </script>
 
 <ArticleList {posts} />
+
+<div class="see-more-button-container">
+  <a href="/" class="see-all-button text-decoration-none">See more from Chistev</a>
+</div>
+
+<style>
+  .see-all-button {
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  color: #242424;
+  padding: 10px 20px;
+  border: 2px solid #242424;
+  border-radius: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  }
+  
+  .see-all-button:hover {
+  background-color: #242424;
+  color: #fff;
+  }
+  </style>
